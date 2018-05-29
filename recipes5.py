13e4dbc73de8aa8ed9e9529fc57b18090f92a11d
Kais Jessa <kais.jessa@ucc.on.ca>
2018-05-29 17:02:58 -0400

    Removed depreciation
    Replaced (line 34):
    sklearn.cross_validation import train_test_split
    with
    from sklearn.model_selection import train_test_split﻿

--- recipes5.py
+++ recipes5.py
@@ -31,7 +31,7 @@ iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test =  train_test_split(X, y, test_size=.5)
 print X_train.shape, X_test.shape
 
